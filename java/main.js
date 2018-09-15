d73e406e90ac0e3124a307af88cddcd8b5f65df3
Christina Houck <31422523+christinahouck@users.noreply.github.com>
2018-09-15 00:30:22 -0700

    Update link in jQuery to function on GitHub pages

--- java/main.js
+++ java/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
    $('.btn-light').click(function() {
      $.ajax({
-       url: 'http://api.openweathermap.org/data/2.5/weather?zip=98225,us&APPID=f25b9be3ce183ae95d1da5570aed3c0e',
+       url: 'https://api.openweathermap.org/data/2.5/weather?zip=98225,us&APPID=f25b9be3ce183ae95d1da5570aed3c0e',
        datatype: 'jsonp',
        success: function(data) {
          $('.weather').html('<p class="data">The weather is currently ' + data.weather[0].description + ' in ' + data.name + '.</p>' );
